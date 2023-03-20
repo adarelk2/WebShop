0e198750cb7266d6d0d923dbc9e40338c68b75d4
adarelk2 <78350796+Adar-Istoreil@users.noreply.github.com>
2023-03-20 21:42:39 +0530

    execute response to client
    this class take care about the response details to client side,
    after we got the response from Application class we are using Response class to send the response to client side

---
+++ API/consts/response.php
@@ -0,0 +1,13 @@
+<?php
+///ENUM OF STATUS/////
+define("STATUS_SUCCESS",200);
+define("STATUS_CREATED",201);
+define("STATUS_DELETED",204);
+define("STATUS_BAD_REQUEST",500);
+
+define("RESPONSE_SUCCESS","!הצלחנו");
+define("RESPONSE_ERROR","הפעולה נכשלה, יש לנסות שוב");
+define("CUSTOMER_HAS_NO_LOANER","ללקוח זה אין מכשיר חלופי");
+define("EMAIL_NOT_SENT","לא הצלחנו לשלוח מייל ללקוח");
+define("NO_PERMISSIONS","אין לך הרשאות עבור פעולה זאת");
+?>
