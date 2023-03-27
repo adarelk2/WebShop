3a96fa14fdb76704f746578eff07a1bfe5b0a673
adarelk2 <78350796+Adar-Istoreil@users.noreply.github.com>
2023-03-28 00:30:48 +0530

    test

---
+++ notify_url.php
@@ -0,0 +1,4 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/db_config.php';
+session_destroy();
+?>
---
+++ suceess_url.php
@@ -0,0 +1,4 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/db_config.php';
+print_r($_SESSION['invoice']);
+?>
