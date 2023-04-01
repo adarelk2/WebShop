1ea710f9939d5071d83bb996f00211631b2c91af
adarelk2 <78350796+Adar-Istoreil@users.noreply.github.com>
2023-04-01 11:18:22 +0530

    payment response - API

--- payment/notify_url.php
+++ payment/notify_url.php
@@ -2,7 +2,14 @@
 ini_set('display_errors', 1);
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config/db_config.php';
-$json = json_encode($_REQUEST);
-$con->query("INSERT into orders(response_json) VALUES('$json')");
+require_once $_SERVER['DOCUMENT_ROOT'] . '/API/consts/orders.php';
+
+$order = $con->query("SELECT * FROM orders where invoice_ext = '$_REQUEST[id]'")->fetch_assoc();
+
+if($order['id'])
+{
+    $con->query("UPDATE `orders` SET `status` = $_REQUEST[status_code] WHERE `orders`.`id` = $order[id]");
+}
+
 session_destroy();
 ?>
